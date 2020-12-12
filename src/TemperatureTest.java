27ff24a5cf7ed03bf76c207550ef823354cd45c4
Diana LN <dianejis@gmail.com>
2020-12-12 16:49:06 +0200

    AQA Homework 06 - Temperature

--- src/TemperatureTest.java
+++ src/TemperatureTest.java
@@ -14,7 +14,9 @@ public class TemperatureTest {
     return new Object[][]{
         {212.0, 100.0},
         {32.0, 0.0},
-        {-15, -26.11111111111111}
+        {-15, -26.11111111111111},
+        {0, -17.77777777777778},
+        {-40.0,-40.0}
         // TODO add 2 more test data here
     };
   }
@@ -28,7 +30,10 @@ public class TemperatureTest {
     return new Object[][]{
         {100.0, 212.0},
         {0.0, 32.0},
-        {-26.11111111111111, -15}
+        {-26.11111111111111, -15},
+        {-17.77777777777778, 0},
+        {11,51.8}
+
         // TODO add 2 more test data here
     };
   }
